25d237bcead7e2dcee4d9ef5613f48eba30b8ec8
Realion033 <purumisky@gmail.com>
2025-01-23 00:57:18 +0900

    feat : Camera Y move

--- NoNameGunProject/Assets/@Works/01_Scripts/Entities/EntityMover.cs
+++ NoNameGunProject/Assets/@Works/01_Scripts/Entities/EntityMover.cs
@@ -1,90 +1,105 @@
 using System;
-using NoNameGun.Entities;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace NoNameGun
+namespace NoNameGun.Entities
 {
     public class EntityMover : NetworkBehaviour, IEntityComponent
     {
+        //움직임 감지 액션 (추후 필요할수도?)
         public event Action<Vector2> OnMovement;
-        //Todo : FSM으로 분류
+
         [SerializeField] private PlayerInputSO _inputSO;
 
-        [Header("Collision detect")]
+        [Header("Collision Detection")]
         [SerializeField] private Transform _groundCheckTrm;
         [SerializeField] private Vector3 _checkerSize;
         [SerializeField] private LayerMask _whatIsGround;
 
-        [Header("Stat")]
-        //Todo : StatSystem
+        [Header("Movement Stats")]
         [SerializeField] private float _moveSpeed = 5f;
+        [SerializeField] private float _mouseSensitivity = 1f;
+
+        [Header("Camera Settings")]
+        [SerializeField] private Transform _cameraTransform;
+        [SerializeField] private float _cameraPitchLimit = 80f;
 
         private Entity _entity;
         private Rigidbody _rbCompo;
-        private Vector2 _moveVelo;
+
+        private Vector2 _previousInputDir;
+        private float _previousMouseDeltaX;
+        private float _cameraPitch;
 
         public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
         public NetworkVariable<Quaternion> Rotation = new NetworkVariable<Quaternion>();
 
-        //컴포넌트 Init
+        // 컴포넌트 초기화
         public void Init(Entity entity)
         {
             _entity = entity;
-
             _rbCompo = _entity.GetComponent<Rigidbody>();
         }
 
         private void FixedUpdate()
         {
-            if (!IsOwner)
-            {
-                return;
-            }
+            if (!IsOwner) return;
 
+            // 현재 입력값 가져오기
             Vector2 inputDir = _inputSO.InputDir;
-            float mouseDeltaX = _inputSO.MouseDelta.x;
-            SubmitMoveInputServerRpc(inputDir, mouseDeltaX);
+            Vector2 mouseDelta = _inputSO.MouseDelta;
+
+            // 주기적으로 서버로 입력 값 전송
+            SubmitMoveInputServerRpc(inputDir, mouseDelta.x);
+
+            // 클라이언트 로컬에서 카메라 회전 처리 (위아래)
+            HandleCameraRotation(mouseDelta.y);
         }
 
-
-        private void DebugCode()
-        {
-            Debug.Log(IsGroundDetected());
-        }
 
         [ServerRpc]
         private void SubmitMoveInputServerRpc(Vector2 inputDir, float mouseDeltaX)
         {
-            // Perform movement on the server
             Move(inputDir, mouseDeltaX);
 
-            // Update NetworkVariables to sync position and rotation with all clients
-            Position.Value = transform.position;
-            Rotation.Value = transform.rotation;
+            if (Vector3.Distance(Position.Value, transform.position) > 0.01f ||
+                Quaternion.Angle(Rotation.Value, transform.rotation) > 0.1f)
+            {
+                Position.Value = transform.position;
+                Rotation.Value = transform.rotation;
+            }
         }
 
         private void Move(Vector2 inputDir, float mouseDeltaX)
         {
+            // 이동 처리
             Vector3 moveDirection = transform.forward * inputDir.y + transform.right * inputDir.x;
             _rbCompo.MovePosition(_rbCompo.position + moveDirection.normalized * _moveSpeed * Time.fixedDeltaTime);
 
-            // Rotate based on mouse input
-            transform.Rotate(Vector3.up * mouseDeltaX);
+            // 캐릭터 회전 처리 (좌우)
+            Quaternion deltaRotation = Quaternion.Euler(0f, mouseDeltaX * _mouseSensitivity, 0f);
+            _rbCompo.MoveRotation(_rbCompo.rotation * deltaRotation);
 
-            // Invoke movement event
+            // 이동 이벤트 발생
             OnMovement?.Invoke(moveDirection.normalized * _moveSpeed);
         }
 
-        public void SetMovement(Vector2 movement)
+        private void HandleCameraRotation(float mouseDeltaY)
         {
-            _moveVelo = movement;
+            if (_cameraTransform == null) return;
+
+            // 카메라 피치 계산 (위아래 회전 제한)
+            _cameraPitch -= mouseDeltaY * _mouseSensitivity;
+            _cameraPitch = Mathf.Clamp(_cameraPitch, -_cameraPitchLimit, _cameraPitchLimit);
+
+            // 카메라 회전 적용
+            _cameraTransform.localRotation = Quaternion.Euler(_cameraPitch, 0f, 0f);
         }
 
         public virtual bool IsGroundDetected()
             => Physics.BoxCast(_groundCheckTrm.position, _checkerSize * 0.5f, Vector3.down,
-            Quaternion.identity, _checkerSize.y, _whatIsGround);
+                Quaternion.identity, _checkerSize.y, _whatIsGround);
 
 #if UNITY_EDITOR
         private void OnDrawGizmosSelected()
@@ -93,15 +108,10 @@ namespace NoNameGun
 
             if (_groundCheckTrm != null)
             {
-                // BoxCast에 맞춘 위치 및 크기
                 Vector3 boxCenter = _groundCheckTrm.position - Vector3.up * (_checkerSize.y * 0.5f);
-                Vector3 boxSize = new Vector3(_checkerSize.x, _checkerSize.y, _checkerSize.z);
-
-                // 박스를 시각화
-                Gizmos.DrawWireCube(boxCenter, boxSize);
+                Gizmos.DrawWireCube(boxCenter, _checkerSize);
             }
         }
 #endif
-
     }
 }
