94dde816f622eb90994c54c2e819a28b08547429
Realion033 <purumisky@gmail.com>
2025-05-16 20:24:31 +0900

    feat : GunBase & Asset Import

--- NoNameGunProject/Assets/@Works/01_Scripts/Weapon/GunBase.cs
+++ NoNameGunProject/Assets/@Works/01_Scripts/Weapon/GunBase.cs
@@ -1,12 +1,28 @@
 using UnityEngine;
+using NoNameGun.Players;
 
 namespace NoNameGun.Weaopon
 {
-    public class GunBase : InitBase
+    public abstract class GunBase : InitBase
     {
+        protected Player _player;
+
         private void Awake()
         {
             Init();
+
+            if (_init == false)
+            {
+                return;
+            }
+
+            _player = GetComponentInParent<Player>();
         }
+        public virtual void Zoom() { }
+
+
+        public abstract void Fire();
+        public abstract void Reload();
+
     }
 }
